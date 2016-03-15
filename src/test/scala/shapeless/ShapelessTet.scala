e9e88203a7d8dcd417221c1688088f2675da472c
objektwerks <objektwerks@gmail.com>
2016-03-15 16:10:54 -0400

    implemented hlist test

--- src/test/scala/shapeless/ShapelessTet.scala
+++ src/test/scala/shapeless/ShapelessTet.scala
@@ -2,6 +2,16 @@ package shapeless
 
 import org.scalatest.FunSuite
 
-class ShapelessTet extends FunSuite {
+case class Value(value: String)
 
+class ShapelessTet extends FunSuite {
+  test("hlist") {
+    val values = 13 :: "labor" :: Value("work") :: HNil
+    val n :: s :: c :: HNil = values
+    assert(values.head == 13)
+    assert(values.tail == "labor" :: Value("work") :: HNil)
+    assert(n == values.select[Int] && n == 13)
+    assert(s == values.select[String] && s == "labor")
+    assert(c == values.select[Value] && c == Value("work"))
+  }
 }
