97bc2085964fa850aa48dfc03eb52faf2ed0c4fc
objektwerks <objektwerks@gmail.com>
2016-03-17 08:24:02 -0400

    refactored test using matchers

--- src/test/scala/shapeless/ShapelessTest.scala
+++ src/test/scala/shapeless/ShapelessTest.scala
@@ -1,17 +1,20 @@
 package shapeless
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
 case class Value(value: String)
 
-class ShapelessTest extends FunSuite {
+class ShapelessTest extends FunSuite with Matchers {
   test("hlist") {
     val values = 13 :: "labor" :: Value("work") :: HNil
     val i :: s :: cc :: HNil = values
-    assert(values.head == 13)
-    assert(values.tail == "labor" :: Value("work") :: HNil)
-    assert(i == values.select[Int] && i == 13)
-    assert(s == values.select[String] && s == "labor")
-    assert(cc == values.select[Value] && cc == Value("work"))
+    values.head shouldBe 13
+    values.tail shouldBe "labor" :: Value("work") :: HNil
+    i shouldBe values.select[Int]
+    i shouldBe 13
+    s shouldBe values.select[String]
+    s shouldBe "labor"
+    cc shouldBe values.select[Value]
+    cc shouldBe Value("work")
   }
 }
