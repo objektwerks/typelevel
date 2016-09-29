b57d9bfaa5f4943288a4354f00422db73d3c2505
objektwerks <objektwerks@gmail.com>
2016-09-29 15:18:59 -0400

    removed space

--- src/test/scala/cat/TypeClassesTest.scala
+++ src/test/scala/cat/TypeClassesTest.scala
@@ -6,6 +6,6 @@ import org.scalatest.{FunSuite, Matchers}
 
 class TypeClassesTest extends FunSuite with Matchers {
   test("semigroup") {
-    Semigroup[Int].combine(1, 2)  shouldBe 3
+    Semigroup[Int].combine(1, 2) shouldBe 3
   }
 }
