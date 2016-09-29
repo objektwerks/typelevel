98eba4c0620e5ce1ea60619633f75bc7c88eed31
objektwerks <objektwerks@gmail.com>
2016-09-29 15:22:30 -0400

    renamed package and test class

--- src/test/scala/cats/TypeClassTest.scala
+++ src/test/scala/cats/TypeClassTest.scala
@@ -1,10 +1,9 @@
-package cat
+package cats
 
-import cats._
 import cats.implicits._
 import org.scalatest.{FunSuite, Matchers}
 
-class TypeClassesTest extends FunSuite with Matchers {
+class TypeClassTest extends FunSuite with Matchers {
   test("semigroup") {
     Semigroup[Int].combine(1, 2) shouldBe 3
   }
