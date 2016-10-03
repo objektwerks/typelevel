1cdbb6658eb54a9488157be5c10d4f8cbf82f41e
objektwerks <objektwerks@gmail.com>
2016-10-03 06:56:29 -0400

    adde monoid test

--- src/test/scala/cats/TypeClassTest.scala
+++ src/test/scala/cats/TypeClassTest.scala
@@ -8,4 +8,9 @@ class TypeClassTest extends FunSuite with Matchers {
     Semigroup[Int].combine(1, 2) shouldBe 3
     Option(1) |+| Option(2) shouldBe Some(3)
   }
+
+  test("monoid") {
+    Monoid[Int].combine(1, 2) shouldBe 3
+    Monoid[Int].combineAll(List(1, 2)) shouldBe 3
+  }
 }
