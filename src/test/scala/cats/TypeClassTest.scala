80139d0af2d37fa8b4a5b3c9a3f4a4a2f7466bd4
objektwerks <objektwerks@gmail.com>
2016-10-03 08:06:06 -0400

    added functor test

--- src/test/scala/cats/TypeClassTest.scala
+++ src/test/scala/cats/TypeClassTest.scala
@@ -11,6 +11,20 @@ class TypeClassTest extends FunSuite with Matchers {
 
   test("monoid") {
     Monoid[Int].combine(1, 2) shouldBe 3
-    Monoid[Int].combineAll(List(1, 2)) shouldBe 3
+    Monoid[Int].combineAll( List(1, 2) ) shouldBe 3
+  }
+
+  test("functor") {
+    Functor[List].map( List(1, 2) ) (_ + 1) shouldBe List(2, 3)
+    Functor[List].fproduct( List(1, 2) ) (_ + 1) shouldBe List( (1, 2), (2, 3))
+    Functor[List].fproduct( List(1, 2) ) (_ + 1).toMap shouldBe Map( 1 -> 2, 2 -> 3 )
+  }
+
+  test("applicative") {
+
+  }
+
+  test("monad") {
+
   }
 }
