f503b9812340778cb7d324f6bcf7cc8a6ddd313e
objektwerks <objektwerks@gmail.com>
2016-10-04 12:55:27 -0400

    renamed test

--- src/test/scala/cats/DataTypeTest.scala
+++ src/test/scala/cats/DataTypeTest.scala
@@ -2,7 +2,7 @@ package cats
 
 import org.scalatest.{FunSuite, Matchers}
 
-class ValidatedTest extends FunSuite with Matchers {
+class DataTypeTest extends FunSuite with Matchers {
   test("validated") {
 
   }
