c527855db417550ec134d2c58cb53cb5c9250904
objektwerks <objektwerks@gmail.com>
2016-10-05 07:05:18 -0400

    renamed and moved test

--- src/test/scala/CatsTest.scala
+++ src/test/scala/CatsTest.scala
@@ -1,10 +1,8 @@
-package cat
-
 import cats._
 import cats.implicits._
 import org.scalatest.{FunSuite, Matchers}
 
-class TypeClassTest extends FunSuite with Matchers {
+class CatsTest extends FunSuite with Matchers {
   test("semigroup") {
     Semigroup[Int].combine(1, 2) shouldBe 3
     Option(1) |+| Option(2) shouldBe Some(3)
--- src/test/scala/cat/DataTypeTest.scala
+++
@@ -1,9 +0,0 @@
-package cat
-
-import org.scalatest.{FunSuite, Matchers}
-
-class DataTypeTest extends FunSuite with Matchers {
-  test("validated") {
-
-  }
-}
