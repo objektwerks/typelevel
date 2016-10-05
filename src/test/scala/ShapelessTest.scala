68295482bedce16754e88c818a33dcfd7267b20d
objektwerks <objektwerks@gmail.com>
2016-10-05 07:48:52 -0400

    enhanced shapeless test

---
+++ src/test/scala/ShapelessTest.scala
@@ -0,0 +1,12 @@
+import org.scalatest.{FunSuite, Matchers}
+import shapeless.HNil
+
+class ShapelessTest extends FunSuite with Matchers {
+  test("hlist") {
+    val hlist = 33 :: "test" :: HNil
+    hlist.head shouldBe 33
+    hlist.tail shouldBe "test" :: HNil
+    hlist.select[Int] shouldBe 33
+    hlist.select[String] shouldBe "test"
+  }
+}
