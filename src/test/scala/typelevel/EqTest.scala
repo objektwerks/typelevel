c977a2a4c34b1c500a416775d25569f974c70239
objektwerks <objektwerks@gmail.com>
2016-11-07 15:42:38 -0500

    add test

---
+++ src/test/scala/typelevel/EqTest.scala
@@ -0,0 +1,15 @@
+package typelevel
+
+import org.scalatest.FunSuite
+
+// Can't test === and =!= due to Scalatest conflict.
+class EqTest extends FunSuite {
+  test("instances") {
+    import cats.Eq
+    import cats.instances.int._
+
+    val eqInt = Eq[Int]
+    assert(eqInt.eqv(1, 1))
+    assert(eqInt.neqv(1, 2))
+  }
+}
