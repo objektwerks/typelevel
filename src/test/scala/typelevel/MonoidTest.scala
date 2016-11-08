c852321f085231e9dc636e03124b6950ac485469
objektwerks <objektwerks@gmail.com>
2016-11-08 08:21:13 -0500

    added monoid syntax test

--- src/test/scala/typelevel/MonoidTest.scala
+++ src/test/scala/typelevel/MonoidTest.scala
@@ -14,4 +14,13 @@ class MonoidTest extends FunSuite {
     assert(Monoid[Int].combine(1, 2) == 3)
     assert(Monoid[Option[Int]].combine(1.some, 2.some) == 3.some)
   }
+  
+  test("syntax") {
+    import cats.Monoid
+    import cats.syntax.semigroup._
+    import cats.instances.int._
+    
+    val x = 1 |+| 2 |+| Monoid[Int].empty
+    assert(x == 3)
+  }
 }
