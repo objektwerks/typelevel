e6f83e20b66333a6a0a250a54a8a18f4233ecb76
objektwerks <objektwerks@gmail.com>
2016-11-08 08:12:20 -0500

    added monoid test

---
+++ src/test/scala/typelevel/MonoidTest.scala
@@ -0,0 +1,17 @@
+package typelevel
+
+import org.scalatest.FunSuite
+
+class MonoidTest extends FunSuite {
+  test("instances") {
+    import cats.Semigroup
+    import cats.Monoid
+    import cats.instances.int._
+    import cats.instances.option._
+    import cats.syntax.option._
+    
+    assert(Semigroup[Int].combine(1, 2) == 3)
+    assert(Monoid[Int].combine(1, 2) == 3)
+    assert(Monoid[Option[Int]].combine(1.some, 2.some) == 3.some)
+  }
+}
