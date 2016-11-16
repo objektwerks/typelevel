7f7b77b5027ebf00da41a3913f3d6e2df326eb7a
objektwerks <objektwerks@gmail.com>
2016-11-16 11:46:52 -0500

    added foldable test

---
+++ src/test/scala/typelevel/FoldableTest.scala
@@ -0,0 +1,15 @@
+package typelevel
+
+import org.scalatest.FunSuite
+
+class FoldableTest extends FunSuite {
+  test("instances") {
+    import cats.Foldable
+    import cats.instances.option._
+    import cats.instances.list._
+    import cats.syntax.option._
+
+    assert(Foldable[Option].foldLeft(3.some, 3)(_ + _) == 6)
+    assert(Foldable[List].foldLeft(List(1, 2, 3), 0)(_ + _) == 6)
+  }
+}
