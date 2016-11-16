3ea36b1153685cef245afecaceaab68b408d3db7
objektwerks <objektwerks@gmail.com>
2016-11-16 08:08:39 -0500

    added monan transformer test

---
+++ src/test/scala/typelevel/MonadTransformerTest.scala
@@ -0,0 +1,16 @@
+package typelevel
+
+import org.scalatest.FunSuite
+
+class MonadTransformerTest extends FunSuite {
+  test("transform") {
+    import cats.data.OptionT
+    import cats.instances.list._
+    import cats.syntax.applicative._
+    import cats.syntax.option._
+
+    type ListOption[A] = OptionT[List, A]
+    val result: ListOption[Int] = 3.pure[ListOption]
+    assert(result.value == List(3.some))
+  }
+}
