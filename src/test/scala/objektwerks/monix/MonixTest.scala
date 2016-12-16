e9af7ad717b8d85c0694d67c5121236d656fd0c7
objektwerks <objektwerks@runbox.com>
2016-12-16 16:36:31 -0500

    limited await to 100ms

--- src/test/scala/objektwerks/monix/MonixTest.scala
+++ src/test/scala/objektwerks/monix/MonixTest.scala
@@ -11,6 +11,6 @@ class MonixTest extends FunSuite with Matchers {
   test("task") {
     val task = Task { 1 + 2 }
     val future = task.runAsync
-    Await.result(future, 1.second) shouldBe 3
+    Await.result(future, 100.millis) shouldBe 3
   }
 }
