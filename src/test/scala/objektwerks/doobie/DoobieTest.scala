139bfa61457e4a3e39cbbbbad71310ce1e23975a
objektwerks <objektwerks@runbox.com>
2017-01-27 06:42:00 -0500

    attempting to load/exec ddl file via doobie

--- src/test/scala/objektwerks/doobie/DoobieTest.scala
+++ src/test/scala/objektwerks/doobie/DoobieTest.scala
@@ -8,10 +8,9 @@ import org.scalatest.{FunSuite, Matchers}
 import scala.io.Source
 
 class DoobieTest extends FunSuite with Matchers {
-  val schema = Source.fromInputStream(getClass.getResourceAsStream("/schema.sql")).mkString // how use in schema test?
   val db = DriverManagerTransactor[IOLite]( "org.h2.Driver", "jdbc:h2:mem:test", "", "" )
 
-  test("schema") {
+  test("embedded ddl") {
     val drop: Update0 =
       sql"""
            drop table if exists worker;
@@ -23,5 +22,11 @@ class DoobieTest extends FunSuite with Matchers {
            create table task (id int primary key not null, workerId int not null, task varchar(128) not null, foreign key(workerId) references worker (id));
         """.update
     (drop.run *> create.run).transact(db).unsafePerformIO
+  }
+
+  test("file ddl") {
+    val schema = Source.fromInputStream(getClass.getResourceAsStream("/schema.sql")).mkString
+    val ddl: Update0 = sql"$schema".update
+    ddl.run.transact(db).unsafePerformIO
   }
 }
