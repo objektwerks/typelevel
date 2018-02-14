ad739ff5856fa57fe38682e0ff532d7d7ed5af45
objektwerks <objektwerks@runbox.com>
2018-02-14 16:07:58 -0500

    refactored validated test

--- src/test/scala/objektwerks/validated/ValidatedTest.scala
+++ src/test/scala/objektwerks/validated/ValidatedTest.scala
@@ -5,28 +5,16 @@ import org.scalatest.{FunSuite, Matchers}
 class ValidatedTest extends FunSuite with Matchers {
   test("instances") {
     import cats.data.Validated
-    import cats.data.Validated.Valid
-    import cats.data.Validated.Invalid
+    import cats.data.Validated.{Invalid, Valid}
+    import cats.syntax.validated._
 
     Validated.Valid(3) shouldEqual Valid(3)
     Validated.Invalid("three") shouldEqual Invalid("three")
     Validated.valid[String, Int](3) shouldEqual Valid(3)
     Validated.invalid[String, Int]("three") shouldEqual Invalid("three")
-  }
-
-  test("syntax") {
-    import cats.syntax.validated._
-    import cats.data.Validated.Valid
-    import cats.data.Validated.Invalid
 
     3.valid[String] shouldEqual Valid(3)
     "three".invalid[Int] shouldEqual Invalid("three")
-  }
-
-  test("methods") {
-    import cats.data.Validated
-    import cats.syntax.validated._
-    import cats.data.Validated.Valid
 
     Validated.catchOnly[NumberFormatException]("three".toInt).isInvalid shouldBe true
     Validated.catchNonFatal(sys.error("Nonfatal")).isInvalid shouldBe true
