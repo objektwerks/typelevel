24c16139f57e37ee286c7f2ca88681cffd47bd55
objektwerks <objektwerks@runbox.com>
2018-02-16 11:03:24 -0500

    enhanced now http4s app

--- src/main/scala/objektwerks/app/NowHttp4sApp.scala
+++ src/main/scala/objektwerks/app/NowHttp4sApp.scala
@@ -1,8 +1,32 @@
 package objektwerks.app
 
-import cats.effect.IO
-import org.http4s.server.ServerApp
+import java.time.LocalTime
 
-object NowHttp4sApp extends ServerApp[IO] {
+import cats.effect._
+import fs2.StreamApp.ExitCode
+import fs2.{Stream, StreamApp}
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.dsl.impl.Root
+import org.http4s.dsl.io._
+import org.http4s.server.blaze._
 
+case class Now(time: String = LocalTime.now.toString)
+object Now {
+  implicit val nowDecoder = jsonOf[IO, Now]
+}
+
+object NowService {
+  val service = HttpService[IO] {
+    case GET -> Root / "now" => Ok(Now().asJson)
+  }
+}
+
+object NowHttp4sApp extends StreamApp[IO] {
+  import NowService._
+
+  override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] =
+    BlazeBuilder[IO].bindHttp(7777).mountService(service, "/").serve
 }
