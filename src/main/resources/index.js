5d7e29c9ad66933a83cd0f2accddba18d01d6d25
objektwerks <objektwerks@runbox.com>
2018-02-16 16:38:57 -0500

    refactored index.html and index.js

--- src/main/resources/index.js
+++ src/main/resources/index.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
-    console.log("document is ready!")
     $("#button-now").click(function() {
-        console.log("button-now clicked!")
         var request = $.ajax({
             url: "/now",
             dataType: "json",
             cache: false
         });
-        request.done(function(data, textStatus, jqXHR) {
-            console.log("data: " + data + " : textStatus: " + textStatus + " : jqXHR: " + jqXHR);
-            $("#text-now").text(jqXHR.responseJSON);
+        request.done(function(now, status, xhr) {
+            $("#text-now").text(now.time);
         });
-        request.fail(function(jqXHR, textStatus, errorThrown) {
-            console.log("jqXHR: " + jqXHR + " : textStatus: " + textStatus + " : errorThrown" + errorThrown);
+        request.fail(function(xhr, status, error) {
+            console.log("xhr: " + xhr + " : status: " + status + " : error" + error);
         });
     });
 })
