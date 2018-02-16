de99f6510bfd771f22aea0b03fc1d5494395c359
objektwerks <objektwerks@runbox.com>
2018-02-16 14:57:02 -0500

    corrected js syntax

--- src/main/resources/index.js
+++ src/main/resources/index.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
     $("#button.now").on("click", function(event) {
-    $.ajax({
-        url: "/now",
-        cache: false
-    }).done(function(now) {
-        alert("current time: " + now);
-        $("#text.now").text(now);
-    }).fail(function(xhr, failure) {
-        alert( "failure: " + failure);
+        var request = $.ajax({
+            url: "/now",
+            cache: false
+        });
+        request.done(function(now) {
+            alert("current time: " + now);
+            $("#text.now").text(now);
+        });
+        request.fail(function(xhr, failure) {
+            alert( "failure: " + failure);
+        });
     });
-});
+})
