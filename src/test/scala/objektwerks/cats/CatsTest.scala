fc5addb8260b4ee68f178389f7edcfe8c8101505
objektwerks <objektwerks@runbox.com>
2018-02-26 14:56:06 -0500

    enhanced monoid cats test; moved validated test to cats test

--- src/test/scala/objektwerks/cats/CatsTest.scala
+++ src/test/scala/objektwerks/cats/CatsTest.scala
@@ -39,6 +39,7 @@ class CatsTest extends FunSuite with Matchers {
     import cats.syntax.monoid._
 
     Monoid[Int].combine(1, 2) shouldEqual 3
+    1 |+| 2 shouldEqual 3
     1 |+| 2 |+| Monoid[Int].empty shouldEqual 3
   }
 
@@ -63,5 +64,26 @@ class CatsTest extends FunSuite with Matchers {
 
     Monad[Option].flatMap(3.some)(i => (i * 3).some).sum shouldEqual 9
     Monad[Option].map(3.some)(_ * 3) shouldEqual 9.some
+  }
+
+  test("validated") {
+    import cats.data.Validated
+    import cats.data.Validated.{Invalid, Valid}
+    import cats.syntax.validated._
+
+    Validated.Valid(3) shouldEqual Valid(3)
+    Validated.Invalid("three") shouldEqual Invalid("three")
+    Validated.valid[String, Int](3) shouldEqual Valid(3)
+    Validated.invalid[String, Int]("three") shouldEqual Invalid("three")
+
+    3.valid[String] shouldEqual Valid(3)
+    "three".invalid[Int] shouldEqual Invalid("three")
+
+    Validated.catchOnly[NumberFormatException]("three".toInt).isInvalid shouldBe true
+    Validated.catchNonFatal(sys.error("Nonfatal")).isInvalid shouldBe true
+    Validated.fromTry(scala.util.Try("three".toInt)).isInvalid shouldBe true
+    Validated.fromEither[String, Int](Left("Error")).isInvalid shouldBe true
+    Validated.fromOption[String, Int](None, "Error").isInvalid shouldBe true
+    3.valid.map(_ * 3) shouldEqual Valid(9)
   }
 }
