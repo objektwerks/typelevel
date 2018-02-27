c75470178c9b3007978a9047b88544efbe0b7129
objektwerks <objektwerks@runbox.com>
2018-02-27 06:55:26 -0500

    edited validated test

--- src/test/scala/objektwerks/cats/CatsTest.scala
+++ src/test/scala/objektwerks/cats/CatsTest.scala
@@ -99,16 +99,16 @@ class CatsTest extends FunSuite with Matchers {
     import cats.data.Validated.{Invalid, Valid}
     import cats.syntax.validated._
 
-    valid[String, Int](3) shouldEqual Valid(3)
-    invalid[String, Int]("three") shouldEqual Invalid("three")
+    valid[String, Int](3) shouldBe Valid(3)
+    invalid[String, Int]("three") shouldBe Invalid("three")
     catchOnly[NumberFormatException]("three".toInt).isInvalid shouldBe true
     catchNonFatal(sys.error("Nonfatal")).isInvalid shouldBe true
     fromTry(Try("three".toInt)).isInvalid shouldBe true
     fromEither[String, Int](Left("Error")).isInvalid shouldBe true
     fromOption[String, Int](None, "Error").isInvalid shouldBe true
 
-    3.valid.map(_ * 3) shouldEqual Valid(9)
-    3.valid[String] shouldEqual Valid(3)
-    "three".invalid[Int] shouldEqual Invalid("three")
+    3.valid.map(_ * 3) shouldBe Valid(9)
+    3.valid[String] shouldBe Valid(3)
+    "three".invalid[Int] shouldBe Invalid("three")
   }
 }
