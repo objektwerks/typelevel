d06cf9c0de26035f18b2c2071bc0f043d2d21233
objektwerks <objektwerks@runbox.com>
2018-03-02 15:10:58 -0500

    renamed app cache; edited index service

--- src/main/scala/objektwerks/app/NowHttp4sApp.scala
+++ src/main/scala/objektwerks/app/NowHttp4sApp.scala
@@ -34,12 +34,12 @@ object Services {
   import Headers._
 
   val indexService = HttpService[IO] {
-    case _ @ GET -> Root => Ok("/index.html")
+    case GET -> Root => Ok("/index.html")
   }
   val indexServiceWithNoCacheHeader = Headers(indexService, noCacheHeader)
 
   val resourceService = HttpService[IO] {
-    case request @ GET -> Root / path if List(".css", ".js", ".ico", ".appcache").exists(path.endsWith) =>
+    case request @ GET -> Root / path if List(".cache", ".ico", ".css", ".js").exists(path.endsWith) =>
       StaticFile.fromResource("/" + path, Some(request)).getOrElseF(NotFound())
   }
   val resourceServiceWithNoCacheHeader = Headers(resourceService, noCacheHeader)
