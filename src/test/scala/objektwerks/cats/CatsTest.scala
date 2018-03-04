69855540febc6c352d2b16cfe1b575d8e0125e30
objektwerks <objektwerks@runbox.com>
2018-03-04 09:40:42 -0500

    added monad error test

--- src/test/scala/objektwerks/cats/CatsTest.scala
+++ src/test/scala/objektwerks/cats/CatsTest.scala
@@ -135,6 +135,22 @@ class CatsTest extends FunSuite with Matchers {
     3.asRight.ensure("Must be equal to 3.")(_ == 3).isRight shouldBe true
   }
 
+  test("monad error") {
+    import cats.MonadError
+    import cats.instances.either._
+    import cats.syntax.applicative._
+    import cats.syntax.applicativeError._
+
+    type ErrorOr[A] = Either[String, A]
+    val monadError = MonadError[ErrorOr, String]
+
+    monadError.pure(3).isRight shouldBe true
+    monadError.raiseError("Error").isLeft shouldBe true
+    monadError.ensure(monadError.pure(3))("Must equal 3")(_ == 3).isRight shouldBe true
+    3.pure[ErrorOr].isRight shouldBe true
+    "Error".raiseError[ErrorOr, Int].isLeft shouldBe true
+  }
+
   test("validated") {
     import cats.data.Validated._
     import cats.data.Validated.{Invalid, Valid}
