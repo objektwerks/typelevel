78dddd0be7c3b173f801c759bbe7099b9a4159cd
objektwerks <objektwerks@runbox.com>
2018-03-06 08:28:21 -0500

    added reader monad test

--- src/test/scala/objektwerks/cats/CatsTest.scala
+++ src/test/scala/objektwerks/cats/CatsTest.scala
@@ -128,6 +128,21 @@ class CatsTest extends FunSuite with Matchers {
     year shouldEqual 1939
   }
 
+  test("reader monad") {
+    import cats.data.Reader
+
+    case class Name(value: String)
+    val nameReader: Reader[Name, String] = Reader(name => name.value)
+    nameReader.run(Name("Fred")).toString shouldEqual "Fred"
+    val greetingReader: Reader[Name, String] = nameReader.map(value => s"Hello, $value!")
+    greetingReader.run(Name("Barney")).toString shouldEqual "Hello, Barney!"
+    val nameGreetingReader: Reader[Name, String] = for {
+      name <- nameReader
+      greeting <- greetingReader
+    } yield s"$name - $greeting"
+    nameGreetingReader.run(Name("Wilma")).toString shouldEqual "Wilma - Hello, Wilma!"
+  }
+
   test("eval monad") {
     import cats.Eval
 
