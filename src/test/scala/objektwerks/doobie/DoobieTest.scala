c60def76d172a858b0bef526ef078f4ce1dc07a4
objektwerks <objektwerks@runbox.com>
2018-09-06 07:26:23 -0400

    replaced fragments with sql

--- src/test/scala/objektwerks/doobie/DoobieTest.scala
+++ src/test/scala/objektwerks/doobie/DoobieTest.scala
@@ -4,6 +4,7 @@ import cats.effect._
 import cats.implicits._
 import doobie._
 import doobie.implicits._
+
 import org.scalatest.FunSuite
 
 import scala.io.Source
@@ -13,7 +14,7 @@ case class Worker(id: Int = 0, name: String)
 case class Task(id: Int = 0, workerId: Int, task: String)
 
 class DoobieTest extends FunSuite {
-  val db = Transactor.fromDriverManager[IO]( "org.h2.Driver", "jdbc:h2:./target/testdb", "", "" )
+  val xa = Transactor.fromDriverManager[IO]( "org.h2.Driver", "jdbc:h2:./target/testdb", "", "" )
   val schema = Source.fromInputStream(getClass.getResourceAsStream("/schema.sql")).mkString
 
   test("ddl > insert > update > select") {
@@ -25,32 +26,32 @@ class DoobieTest extends FunSuite {
 
   def ddl(schema: String): Int = {
     val ddl = Fragment.const(schema).update.run
-    ddl.transact(db).unsafeRunSync()
+    ddl.transact(xa).unsafeRunSync()
   }
 
   def insert: Int = {
-    val barney = Fragment.const("insert into worker(name) values ('barney')").update
-    val fred = Fragment.const("insert into worker(name) values('fred')").update
-    val workers = (barney.run *> fred.run).transact(db).unsafeRunSync()
+    val barney = sql"insert into worker(name) values ('barney')".update
+    val fred = sql"insert into worker(name) values('fred')".update
+    val workers = (barney.run *> fred.run).transact(xa).unsafeRunSync()
 
-    val barneyTask = Fragment.const("insert into task(workerId, task) values(1, 'clean pool')").update
-    val fredTask = Fragment.const("insert into task(workerId, task) values(2, 'clean pool')").update
-    val tasks = (barneyTask.run *> fredTask.run).transact(db).unsafeRunSync()
+    val barneyTask = sql"insert into task(workerId, task) values(1, 'clean pool')".update
+    val fredTask = sql"insert into task(workerId, task) values(2, 'clean pool')".update
+    val tasks = (barneyTask.run *> fredTask.run).transact(xa).unsafeRunSync()
 
     workers + tasks
   }
 
   def update: Int = {
-    val barneyUpdate = Fragment.const("update worker set name = 'barney rebel' where name = 'barney'").update
-    val fredUpdate = Fragment.const("update worker set name = 'fred flintstone' where name = 'fred'").update
-    (barneyUpdate.run *> fredUpdate.run).transact(db).unsafeRunSync()
+    val barneyUpdate = sql"update worker set name = 'barney rebel' where name = 'barney'".update
+    val fredUpdate = sql"update worker set name = 'fred flintstone' where name = 'fred'".update
+    (barneyUpdate.run *> fredUpdate.run).transact(xa).unsafeRunSync()
   }
 
   def select: Int = {
-    val selectWorkers = Fragment.const("select * from worker").query[Worker]
-    val selectTasks = Fragment.const("select * from task").query[Task]
-    val workers = selectWorkers.to[List].transact(db).unsafeRunSync.length
-    val tasks = selectTasks.to[List].transact(db).unsafeRunSync.length
+    val selectWorkers = sql"select * from worker".query[Worker]
+    val selectTasks = sql"select * from task".query[Task]
+    val workers = selectWorkers.to[List].transact(xa).unsafeRunSync.length
+    val tasks = selectTasks.to[List].transact(xa).unsafeRunSync.length
     workers + tasks
   }
 }
