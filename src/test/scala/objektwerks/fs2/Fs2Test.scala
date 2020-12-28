c314ecaec3344144bb8aeb97cf44a7d9594f18f6
objektwerks <objektwerks@runbox.com>
2020-12-28 12:39:46 -0500

    added bracket test to fs2 test

--- src/test/scala/objektwerks/fs2/Fs2Test.scala
+++ src/test/scala/objektwerks/fs2/Fs2Test.scala
@@ -4,6 +4,8 @@ import cats.effect.IO
 
 import fs2.Stream
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
@@ -33,5 +35,13 @@ class Fs2Test extends AnyFunSuite with Matchers {
   test("error") {
     val effect = Stream.raiseError[IO](new Exception("test error"))
     effect.handleErrorWith { e => Stream.emit(e.getMessage) }.compile.toList.unsafeRunSync() shouldBe List("test error")
+  }
+
+  test("bracket") {
+    val count = new AtomicInteger(0)
+    val acquire = IO { count.incrementAndGet; () }
+    val release = IO { count.decrementAndGet; () }
+    Stream.bracket(acquire)(_ => release).flatMap(_ => Stream(1, 2, 3)).compile.toList.unsafeRunSync() shouldBe List(1, 2, 3)
+    count.get shouldBe 0
   }
 }
