b45b14e17b8e4d091bc87a35a53782ee0ccb3cdb
objektwerks <objektwerks@runbox.com>
2020-12-28 08:40:02 -0500

    enhanced pure test

--- src/test/scala/objektwerks/fs2/Fs2Test.scala
+++ src/test/scala/objektwerks/fs2/Fs2Test.scala
@@ -10,15 +10,18 @@ import org.scalatest.matchers.should.Matchers
 class Fs2Test extends AnyFunSuite with Matchers {
   test("pure") {
     Stream.empty.toList shouldBe List()
+
     Stream.emit(1).toList shouldBe List(1)
     Stream.emits(List(1, 2, 3)).toList shouldBe List(1, 2, 3)
     Stream.emits(List(1, 2, 3)).toList.sum shouldBe 6
+
     (Stream(1, 2, 3) ++ Stream(4, 5, 6)).toList shouldBe List(1, 2, 3, 4, 5, 6)
-    Stream(None, Some(2), Some(3)).collect { case Some(i) => i }.toList shouldBe List(2, 3)
-    Stream(1, 2, 3).filter(_ % 2 != 0).toList shouldBe List(1, 3)
+    Stream(Some(1), None, Some(3)).collect { case Some(i) => i }.toList shouldBe List(1, 3)
+
+    Stream(1, 2, 3).filter(_ % 2 == 0).toList shouldBe List(2)
     Stream(1, 2, 3).fold(0)(_ + _).toList shouldBe List(6)
-    Stream(1, 2, 3).map(_ + 1).toList shouldBe List(2, 3, 4)
-    Stream(1, 2, 3).flatMap(i => Stream(i, i)).toList
+    Stream(1, 2, 3).map(i => i * i).toList shouldBe List(1, 4, 9)
+    Stream(1, 2, 3).flatMap(i => Stream(i, i)).toList shouldBe List(1, 1, 2, 2, 3, 3)
   }
 
   test("eval") {
