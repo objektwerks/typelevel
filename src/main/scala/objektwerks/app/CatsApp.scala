7df229e9ca235f39c3cc6ac8ef67a14d94a5db54
objektwerks <objektwerks@runbox.com>
2020-12-29 16:09:52 -0500

    implemented CatsApp with simple current ZonedDateTime impl

--- src/main/scala/objektwerks/app/CatsApp.scala
+++ src/main/scala/objektwerks/app/CatsApp.scala
@@ -2,6 +2,11 @@ package objektwerks.app
 
 import cats.effect.{ExitCode, IO, IOApp}
 
-object CatsApp extends IOApp { // TODO!
-  def run(args: List[String]): IO[ExitCode] = IO { ExitCode.Success }
+import java.time.ZonedDateTime
+
+object CatsApp extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    IO {
+      println(s"The current date and time is: ${ZonedDateTime.now()}")
+    }.as(ExitCode.Success)
 }
