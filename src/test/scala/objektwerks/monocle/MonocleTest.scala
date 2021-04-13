8c700fa828708404300542b2384ec69b004628a9
objektwerks <objektwerks@runbox.com>
2021-04-13 14:37:54 -0400

    added domain model to monocle test

--- src/test/scala/objektwerks/monocle/MonocleTest.scala
+++ src/test/scala/objektwerks/monocle/MonocleTest.scala
@@ -3,6 +3,11 @@ package objektwerks.monocle
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+case class Resident(name: String, street: Street)
+case class Street(name: String, city: City)
+case class City(name: String, state: State)
+case class State(name: String)
+
 class MonocleTest extends AnyFunSuite with Matchers {
   test("lens") {
   }
