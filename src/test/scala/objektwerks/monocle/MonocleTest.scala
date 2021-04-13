02bd728d409eab52439a91a3f6e22b5098f71257
objektwerks <objektwerks@runbox.com>
2021-04-13 13:01:13 -0400

    added monocle empty test

---
+++ src/test/scala/objektwerks/monocle/MonocleTest.scala
@@ -0,0 +1,9 @@
+package objektwerks.monocle
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class MonocleTest extends AnyFunSuite with Matchers {
+  test("lens") {
+  }
+}
