7ab15a5eb7a61b6f2023273696942827a1abe28f
objektwerks <objektwerks@runbox.com>
2021-04-13 14:43:47 -0400

    added lens to monocle test

--- src/test/scala/objektwerks/monocle/MonocleTest.scala
+++ src/test/scala/objektwerks/monocle/MonocleTest.scala
@@ -1,5 +1,8 @@
 package objektwerks.monocle
 
+import monocle.Lens
+import monocle.macros.GenLens
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
@@ -8,7 +11,17 @@ case class Street(name: String, city: City)
 case class City(name: String, state: State)
 case class State(name: String)
 
+object ResidentLens {
+    val resident: Lens[Resident, Street] = GenLens[Resident](_.street)
+    val street: Lens[Street, City] = GenLens[Street](_.city)
+    val city: Lens[City, State] = GenLens[City](_.state)
+}
+
 class MonocleTest extends AnyFunSuite with Matchers {
+  test("copy") {
+
+  }
+  
   test("lens") {
   }
 
