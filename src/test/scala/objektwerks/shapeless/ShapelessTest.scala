0ec995cfe172652fa9d5a527b66ce42c7126ce04
objektwerks <objektwerks@runbox.com>
2021-04-14 09:06:40 -0400

    added shapeless coproduct test

--- src/test/scala/objektwerks/shapeless/ShapelessTest.scala
+++ src/test/scala/objektwerks/shapeless/ShapelessTest.scala
@@ -3,12 +3,27 @@ package objektwerks.shapeless
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import shapeless.HNil
+import shapeless._
 
 class ShapelessTest extends AnyFunSuite with Matchers {
   test("hlist") {
     val hlist = 3 :: "3" :: HNil
     hlist.head shouldBe 3
     hlist.tail shouldBe "3" :: HNil
+  }
+
+  test("coproduct") {
+    object Green
+    object Red
+    object Yellow
+    type Light = Green.type :+: Red.type :+: Yellow.type :+: CNil
+
+    val green: Light = Coproduct[Light](Green)
+    val red: Light = Coproduct[Light](Red)
+    val yellow: Light = Coproduct[Light](Yellow)
+
+    green.select[Green.type] shouldBe Some(Green)
+    red.select[Red.type] shouldBe Some(Red)
+    yellow.select[Yellow.type] shouldBe Some(Yellow)
   }
 }
