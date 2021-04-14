6f1a7f7e1f111ebacfa914bb8c90ec5a7cca7469
objektwerks <objektwerks@runbox.com>
2021-04-14 08:36:18 -0400

    added shapeless test

---
+++ src/test/scala/objektwerks/shapeless/ShapelessTest.scala
@@ -0,0 +1,10 @@
+package objektwerks.shapeless
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class ShapelessTest extends AnyFunSuite with Matchers {
+  test("shapeless") {
+      
+  }
+}
