3c1275fdcfde4d8d915105091d08793b302cde23
objektwerks <objektwerks@runbox.com>
2021-04-14 08:42:54 -0400

    added shapeless hlist test

--- src/test/scala/objektwerks/shapeless/ShapelessTest.scala
+++ src/test/scala/objektwerks/shapeless/ShapelessTest.scala
@@ -3,8 +3,12 @@ package objektwerks.shapeless
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import shapeless.HNil
+
 class ShapelessTest extends AnyFunSuite with Matchers {
-  test("shapeless") {
-      
+  test("hlist") {
+    val hlist = List(3) :: List("3") :: HNil
+    hlist.head shouldBe List(3)
+    hlist.tail shouldBe List("3") :: HNil
   }
 }
